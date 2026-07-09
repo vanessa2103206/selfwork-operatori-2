5e9d11d97bc7f1c5b92722925053dbce4d70f691
vanessa2103206 <vanessac2016@libero.it>
2026-07-09 11:38:44 +0200

    Codice separato con successo in main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// 1. Creo le due variabili di partenza
+let annoCorrente = 2026;
+let annoNascita = 1996; // Puoi mettere il tuo anno di nascita vero per fare il test!
+
+// 2. Calcolo l'eta e gli anni mancanti ai 100
+let eta = annoCorrente - annoNascita;
+let anniMancanti = 100 - eta;
+
+// 3. Mostro la frase esatta richiesta in console
+console.log("Hai " + eta + " anni e ti mancano " + anniMancanti + " anni per compierne 100");
